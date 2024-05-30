8e77ce06a5fb60c7bc79c3ad396384bca7e05532
123nick123 <ygyy8514@gmail.com>
2024-05-30 14:12:15 +0800

    jason
    homework

---
+++ main.py
@@ -0,0 +1,62 @@
+# # name=input("請輸入帳號")
+# # pwd=input("請輸入密碼")
+# # if name=="jason" and pwd=="0543":
+# #     print("恭喜成功登入")
+# #     print('welcome back')
+# # else:
+# #     print("帳密錯誤")
+# # print("程式執行完畢")
+# print(1,2,3,4,sep="",end="")
+# # print(1,2,3,4)
+# print(list(range(1,10,2)))
+# print(list(range(5,10)))
+# print(list(range(10)))
+# print(list(range(2,10,2)))
+
+# for i in range(2,11,2):
+#     print(i)
+#
+# print('-'*50)
+# for i in range(10,0,-1):
+#     print(i)
+# print()
+# print(list(range(10)))
+# i=1
+# while i<10:
+#     print(i)
+#     i+=1
+# total=0
+# for i in range(1,100,2):
+#     print(i)
+#     total+=i
+# print(total)
+# print("finish")
+# total=0
+# for i in range(0,100,7):
+#     print(i)
+#     total+=i
+# print(total)
+#
+# total=0
+# for i in range(0,100,15):
+#     print(i)
+#     total+=i
+# print(total)
+# for a in range(1,10):
+#     for b in range(1,10):
+#         print(a,"*",b,"=",a*b,sep="",end="\t")
+#     print()
+# for i in range(1,6):
+#     for a in range(i):
+#         print(a,end="")
+#     print()
+# for b in range(1,10):
+#     for a in range(9,1,-1):
+#         print(a,"*",b,"=",a*b,sep="",end="\t")
+#     print()
+print("Hello", end="  ")
+print("world")
+a=0
+for i in range(101):
+    if i%2==1:
+        print(i)
